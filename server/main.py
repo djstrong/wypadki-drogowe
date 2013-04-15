aed8020bb4dbff636b35533a836c5f77ddbaff1f
tpawel <tpawel@student.agh.edu.pl>
2013-04-16 01:05:16 +0200

    konfiguracja, wykresy, troche jeszcze zostalo do obsluzenia, ale cos juz dziala

--- server/main.py
+++ server/main.py
@@ -1,9 +1,10 @@
 import web
-
-
+import json
+from operator import itemgetter
 
 urls = (
-    '/', 'Index'
+    '/', 'Index',
+    '/get_data','Data'
 )
 
 db = web.database(dbn='sqlite', db='wypadki.db')
@@ -11,9 +12,43 @@ render = web.template.render('templates/')
 
 class Index(object):
     def GET(self):
-        w = db.select('wypadki',what='wypadki,zabici,ranni',where='miasto="katowice"',order='id')
-        daty = db.select('wypadki',what='data',where='miasto="katowice"',order='id')
-        return render.index(name='ble',wypadki=w.list()[:5],daty=daty.list()[:5])
+        return render.index()
+    
+class Data(object):
+    def GET(self):
+        print "GETTTTT"
+        params = web.input()
+        
+        #wez dane z wypadkow
+        accidents = db.select('wypadki',what='data,wypadki,zabici,ranni',where='miasto="%s" AND data>="%s" AND data<="%s"'%(params['selected_city'],params['date_from'],params['date_to']),order='id')
+        
+        
+        #wez te druge dane
+        table = params['selected_condition'].split(',')[0]
+        param = params['selected_condition'].split(',')[1]
+        conditions = db.select(table,what='data,'+param,where='miasto="%s" AND data>="%s" AND data<="%s"'%(params['selected_city'],params['date_from'],params['date_to']),order='id')
+        
+        
+        result = {}
+        
+        rows = {}
+        for acc in accidents:
+            rows[acc['data']]={'wypadki':acc['wypadki']}
+        for c in conditions:
+            if rows.has_key(c['data']):
+                rows[c['data']][param]=c[param]
+        result['rows']=sorted(rows.items(),key=itemgetter(0))
+        
+        
+        #specjalny przypadek - swieta - co z nim zrobic?
+        
+        
+        #todo: obsluzyc rozne poziomy granulacji 
+        
+        return json.dumps(result)
+        
+        
+        
     
     
 if __name__=="__main__":
