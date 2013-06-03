e0e56cc2fb04d3a22da6186d49e42cf151000863
tpawel <tpawel@student.agh.edu.pl>
2013-06-03 21:01:18 +0200

    jakies wojewodzkie dane - wypadki zbinowane, ale dla kazdego woj. osobno

---
+++ analizy/csv_generator.py
@@ -0,0 +1,46 @@
+import sqlite3
+import csv
+import numpy
+import sys
+from operator import itemgetter
+
+if __name__=="__main__":
+    conn = sqlite3.connect('../server/nowe.db')
+    
+    #czy duzo, czy malo wypadkow na podstawie pogody
+    c = conn.cursor()
+    wojewodztwa = c.execute("SELECT * from wojewodztwa").fetchall()    
+    for w in wojewodztwa:
+        print "WOJEWODZTWO",w
+        print c.execute("SELECT * from moon_phase").fetchone()
+        
+        pogoda = c.execute("""SELECT max_temp,min_temp,cisnienie,zachod-wschod as godzin_dnia,phase,wypadki
+                                FROM pogoda JOIN wypadki ON wypadki.data=pogoda.data
+                                JOIN moon_phase ON moon_phase.data=pogoda.data
+                            WHERE pogoda.data>'2009-01-01' AND pogoda.wojewodztwo=wypadki.wojewodztwo and pogoda.wojewodztwo=%d"""%w[0]).fetchall()
+        print len(pogoda)
+        acc = map(itemgetter(5),pogoda)
+        freq,bins = numpy.histogram(acc)
+        print freq
+        print bins
+        
+
+        positions = numpy.digitize(acc,bins) # numery binow
+        print max(positions)
+        print min(positions)
+    
+        with open('pogoda_%s.csv'%w[1],'w') as file:
+            writer = csv.DictWriter(file, ['max_temp','min_temp','cisnienie','godzin_dnia','faza_ksiezyca','wypadki'])
+            writer.writeheader()
+            for row,pos in zip(pogoda,positions):
+                writer.writerow({'max_temp':row[0],
+                                 'min_temp':row[1],
+                                 'cisnienie':row[2],
+                                 'godzin_dnia':row[3],
+                                 'faza_ksiezyca':row[4],
+                                 #'wypadki':bin_names[pos-1]})
+                                 'wypadki':'%s, %s'%(bins[pos-2],bins[pos-1]) if pos>1 else '< %s'%bins[pos-1]})
+
+            
+    
+    
